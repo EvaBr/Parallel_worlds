3c43c7fdb7d0ce3e18b5a762f5497f6cabc1866f
EvaBr <eva.breznik@student.fmf.uni-lj.si>
2015-05-19 21:34:54 +0200

    solution ws4

--- WS4/student/mandelbrot_set_par.c
+++ WS4/student/mandelbrot_set_par.c
@@ -1,3 +1,8 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <complex.h>
 #include <omp.h>
 
 #include "mandelbrot_set.h"
@@ -8,5 +13,39 @@ void mandelbrot_draw(int x_resolution, int y_resolution, int max_iter,
 	                int palette_shift, unsigned char (*image)[x_resolution][3],
 						 int num_threads) {
 
-	// put your code here
+        double y;
+        double x;
+
+        complex double Z;
+        complex double C;
+
+        int k;
+
+	#pragma omp parallel for private( x, y, Z, C, k) schedule(dynamic)
+        for (int i = 0; i < y_resolution; i++){
+                for (int j = 0; j < x_resolution; j++){
+                        y = view_y1 - i * y_stepsize;
+                        x = view_x0 + j * x_stepsize;
+
+                        Z = 0 + 0 * I;
+                        C = x + y * I;
+
+                        k = 0;
+
+                        do{
+                                Z = Z * Z + C;
+                                k++;
+                        } while (cabs(Z) < 2 && k < max_iter);
+
+                        if (k == max_iter){
+                                memcpy(image[i][j], "\0\0\0", 3);
+                        }
+                        else{
+				int index = (k + palette_shift)
+				%(sizeof(colors) / sizeof(colors[0]));
+                        	memcpy(image[i][j], colors[index], 3);
+                        }
+                }
+        }
 }
+
---
+++ WS4/student/mandelbrot_set_par.d
@@ -0,0 +1,4 @@
+student/mandelbrot_set_par.o: student/mandelbrot_set_par.c \
+ /home/eva/Desktop/Parallel_worlds/WS4/mandelbrot_set.h
+
+/home/eva/Desktop/Parallel_worlds/WS4/mandelbrot_set.h:
